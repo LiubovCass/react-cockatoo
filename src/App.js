f8dcc94a93202984c22649d7ab2258064ceaf0c4
Lyubov Rodin <luba@lyubovs-mbp.lan>
2023-02-25 00:29:03 -0500

    Lesson 1.6 Reusable Components, Imperative React

--- src/App.js
+++ src/App.js
@@ -1,57 +1,61 @@
-import React from 'react';
-import TodoList from './TodoList';
-import AddTodoForm from './AddTodoForm';
-
+import React from "react";
+import TodoList from "./TodoList";
+import AddTodoForm from "./AddTodoForm";
 
 // create custom hook
 function useSemiPersistentState() {
-   
   // Create new state variable named todoTitle with setter setTodoTitle
-   const [todoList, setTodoList] = React.useState(JSON.parse(localStorage.getItem("savedTodoList")) || []);
-  
-   // Define a useEffect React hook with todoList as a dependency
+  const [todoList, setTodoList] = React.useState(
+    JSON.parse(localStorage.getItem("savedTodoList")) || []
+  );
+
+  // Define a useEffect React hook with todoList as a dependency
   // Inside the side-effect handler function, save the todoList inside localStorage with the key "savedTodoList"
-  // convert todoList to a string before saving in localStorage 
+  // convert todoList to a string before saving in localStorage
   React.useEffect(() => {
-  localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
   }, [todoList]);
-  
 
   return [todoList, setTodoList];
-};
+}
 
-
-
-function App () {
-
+function App() {
   const [todoList, setTodoList] = useSemiPersistentState("");
 
   // Remove the newTodo state variable and the corresponding JSX that displays it
 
   // const [newTodo, setNewTodo] = React.useState("");
-//   Inside the App functional component, above the return statement, create a new state variable named newTodo with update function named setNewTodo
-// hint: useState hook
+  //   Inside the App functional component, above the return statement, create a new state variable named newTodo with update function named setNewTodo
+  // hint: useState hook
 
-// Declare a new function named addTodo that takes newTodo as a parameter
-//  Call the setTodoList state setter and use the spread operator to pass the existing Objects in the todoList Array along with the newTodo Object
-function addTodo(newTodo) {
-  setTodoList([...todoList, newTodo])
-}
+  // Declare a new function named addTodo that takes newTodo as a parameter
+  //  Call the setTodoList state setter and use the spread operator to pass the existing Objects in the todoList Array along with the newTodo Object
+  function addTodo(newTodo) {
+    setTodoList([...todoList, newTodo]);
+  }
+  // Define a new handler function named removeTodo with parameter id, remove the item with the given id from todoList
+  // with methods
+  //  Call the setTodoList state setter and pass the new or modified Array
+  const removeTodo = (id) => {
+    const filterTodoList = todoList.filter((todo) => todo.id !== id);
+    setTodoList(filterTodoList);
+  };
   return (
     <>
-      <h1>Todo List</h1> 
-     
+      <h1>Todo List</h1>
+
       {/* Change the value of the onAddTodo prop for AddTodoForm to addTodo */}
 
-     <AddTodoForm onAddTodo={addTodo}/>  
-     {/* Pass setNewTodo as a callback handler prop named onAddTodo to the AddTodoForm component */}
-     
-     {/* Pass todoList state as a prop named todoList to the TodoList component */}
-     <TodoList todoList = {todoList}/>
-     
-     {/* Below the <AddTodoForm /> component, add a paragraph element that displays the value of newTodo variable */}
+      <AddTodoForm onAddTodo={addTodo} />
+      {/* Pass setNewTodo as a callback handler prop named onAddTodo to the AddTodoForm component */}
 
+      {/* Pass todoList state as a prop named todoList to the TodoList component */}
+      {/* <TodoList todoList={todoList} /> */}
 
+      {/* Below the <AddTodoForm /> component, add a paragraph element that displays the value of newTodo variable */}
+      {/* Pass removeTodo as a callback handler prop named onRemoveTodo to the TodoList component */}
+
+      <TodoList onRemoveTodo={removeTodo} todoList={todoList} />
     </>
   );
 }
--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
 
 // update props to use destructuring
-const TodoListItem = ({title}) => {
-    return (
-        <li>{title}</li> 
-    )
-
-}
+const TodoListItem = ({ id, onRemoveTodo, title }) => {
+  return (
+    <>
+      <li key={id}>
+        {title}
+        <button type="button" onClick={() => onRemoveTodo(id)}>
+          Remove
+        </button>
+        {/* Add an onClick prop to the button element and pass a function that calls onRemoveTodo from props with the current item id as an argument */}
+      </li>
+    </>
+  );
+};
 
 export default TodoListItem;
